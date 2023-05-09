bf984eb941e41b76443d098076b8f39d019685e1
Olena Mialyk <olena.mialyk@gmail.com>
2023-05-09 12:20:49 -0700

    Refactoring: remove hardcoded default urls

--- web/src/main/java/com/mialyk/web/controllers/WebController.java
+++ web/src/main/java/com/mialyk/web/controllers/WebController.java
@@ -2,6 +2,7 @@ package com.mialyk.web.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,14 +19,15 @@ public class WebController {
     @Autowired
     private IStateService stateService;
     @Autowired
-    private IMetroAreaService metroAreaService;
-    @Autowired
-    private ICountyService countyService;
-    @Autowired
     private IRegionService regionService;
+    @Autowired
+    private Environment env;
 
-    @Value("${hat.mapsApiKey}")
+    @Value("${hat.google-maps-api-key}")
     private String googleMapsApiKey;
+
+    @Value("${hat.default-state}")
+    private String defaultState;
 
     @GetMapping("/")
     public String getStarter(Model model) {
@@ -34,28 +36,36 @@ public class WebController {
 
     @GetMapping("/charts/")
     public String getPieChart(Model model) {
-        String url = "http://localhost:8080/hat/api/historical/values/state/California";
+
+        String url = String.format("%s/hat/api/historical/values/state/%s", getBaseUrl(), defaultState);
         String historicalData = restTemplate.getForObject(url, String.class);
 
         model.addAttribute("regionTypes", regionService.getRegionTypes());
         model.addAttribute("states", stateService.getStateDtos());
-        model.addAttribute("metros", metroAreaService.getMetroAreaDtos());
-        model.addAttribute("counties", countyService.getCountyDtos());
+        // model.addAttribute("metros", metroAreaService.getMetroAreaDtos());
+        // model.addAttribute("counties", countyService.getCountyDtos());
 
-        //StateDto selectedState = null;
-        //model.addAttribute("defaultState", new StateDto());
         model.addAttribute("defaultStateHistoricalData", historicalData);
-        model.addAttribute("defaultState", "California");
+        model.addAttribute("defaultState", defaultState);
 
         return "charts";
     }
 
     @GetMapping("/maps/")
     public String getActualData(Model model) {
-        String url = "http://localhost:8080/hat/api/analytics/states";
+
+        String url = String.format("%s/hat/api/analytics/states", getBaseUrl());
         String result = restTemplate.getForObject(url, String.class);
 
         model.addAttribute("defaultStateAnalytics", result);
         return "maps";
+    }
+
+    private String getBaseUrl() {
+        String port = env.getProperty("server.port");
+        String host = env.getProperty("server.host");
+        Boolean sslEnabled = env.getProperty("server.ssl.enabled", Boolean.class, false);
+        String protocol = sslEnabled ? "https" : "http";
+        return String.format("%s://%s:%s", protocol, host, port);
     }
 }
