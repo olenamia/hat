7c6751650cbab0f55c60613d472f0fa46a943f0f
Olena Mialyk <olena.mialyk@gmail.com>
2023-05-09 22:19:18 -0700

    Swagger added

--- web/src/main/java/com/mialyk/web/WebApplication.java
+++ web/src/main/java/com/mialyk/web/WebApplication.java
@@ -1,8 +1,10 @@
 package com.mialyk.web;
 
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.mialyk.persistence", "com.mialyk.business.services","com.mialyk.web.controllers","com.mialyk.business.mappers"})
@@ -12,4 +14,12 @@ public class WebApplication {
 		SpringApplication.run(WebApplication.class, args);
         System.out.println("WEB STARTED");
 	}
+
+    @Bean
+    public GroupedOpenApi api() {
+        return GroupedOpenApi.builder()
+                .group("hat-api")
+                .pathsToMatch("/hat/**")
+                .build();
+    }
 }
