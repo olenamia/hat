29606e31dfbfb93983ca039dfe5ae09701dc9948
Olena Mialyk <olena.mialyk@gmail.com>
2023-05-11 22:43:30 -0700

    Add documentation for analytics controller

--- web/src/main/java/com/mialyk/web/controllers/AnalyticsController.java
+++ web/src/main/java/com/mialyk/web/controllers/AnalyticsController.java
@@ -3,6 +3,7 @@ package com.mialyk.web.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 import com.mialyk.business.dtos.AnalyticsDto;
 import com.mialyk.business.services.IHomeValueService;
 
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+
+@Tag(name = "Analytics", description = "Endpoints to get recent trends analytics for different region types")
 @RestController
 @RequestMapping("hat/api/analytics")
 public class AnalyticsController {
@@ -17,7 +24,12 @@ public class AnalyticsController {
     private IHomeValueService homeValueZillowService;
 
     @GetMapping({"/states"})
-    public List<AnalyticsDto> getHomeValuesForStates() { 
-        return homeValueZillowService.GetAnalyticsForStates();
+    @Operation(summary = "Recent trends by state", description = "Recent home value, year-over-year and month-over-month change")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "OK"),
+    })
+    public ResponseEntity<List<AnalyticsDto>> getHomeValuesForStates() { 
+        List<AnalyticsDto> statesAnalytics = homeValueZillowService.GetAnalyticsForStates();
+        return ResponseEntity.ok(statesAnalytics);
     }
 }
